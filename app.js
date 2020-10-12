6288f8c3fc96edef5f1cd0d89f025ca6056cdfc6
DunkaL <49835111+DunkaL@users.noreply.github.com>
2020-10-12 14:16:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: 'bw1.jpg',
+    image2: 'bw2.jpg',
+    // angle2: Math.PI / 2,
+    // speedOut: 2,
+    displacementImage: './diss.png'
+
+});
+
+// https://www.pexels.com/ --сайты с картинками
+
+// https://github.com/robin-dela/hover-effect -- кдо и файлы для анимации
+
+// https://cdnjs.com/libraries/three.js -- скрипты для подключения (какие подключать есть в index.html)
+
+// https://cdnjs.com/libraries/gsap -- скрипт для подключения (какие подключать есть в index.html)
